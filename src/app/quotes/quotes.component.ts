236926415bbc160baedc691cce84d2044b125731
NessaAz <vanessa.azenwa@gmail.com>
2022-04-26 10:24:53 +0300

    upload

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quotes: Quotes [] = [
+    new Quotes(1, 'First Quote', 0, 0),
+    new Quotes(2, 'Second Quote', 0, 0),
+    new Quotes(3, 'Third Quote', 0, 0),
+  ] 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
